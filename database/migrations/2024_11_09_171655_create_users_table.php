96ce23e268d2c91ab6a40456d4123b98a2b039bd
AhmadDoobi <ahmad_doobi@icloud.com>
2024-11-09 21:14:15 +0300

    added a name column to the users table

--- database/migrations/2024_11_09_171655_create_users_table.php
+++ database/migrations/2024_11_09_171655_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
+            $table->string('name');
             $table->string('email')->unique();
             $table->boolean('email_verified')->default(false);
             $table->string('password')->default('guest'); // 'guest' for guest users
